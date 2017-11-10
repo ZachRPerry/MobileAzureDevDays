ff55fb4ac58a0c4b17da51bd0354cf54115c5749
Zach Perry <zachrperry@outlook.com>
2017-11-10 11:58:46 -0500

    api key change

--- Xamarin/MobileAzureDevDays/Services/TextAnalysis.cs
+++ Xamarin/MobileAzureDevDays/Services/TextAnalysis.cs
@@ -10,8 +10,8 @@ namespace MobileAzureDevDays.Services
 {
     static class TextAnalysis
     {
-#error Missing Sentiment API Key
-        const string _sentimentAPIKey = "";
+//#error Missing Sentiment API Key
+        const string _sentimentAPIKey = "adecfb4d82814beda0121c73c33fb78c";
 
         readonly static Lazy<SentimentClient> _sentimentClientHolder = new Lazy<SentimentClient>(() => new SentimentClient(_sentimentAPIKey));
 
